90f0551a2a40283cf37e51946ee3616ef796734e
Jordan Kohl <kohl.jordan@gmail.com>
2014-07-02 13:43:35 -0700

    updated to Heroku host and port

--- config.js
+++ config.js
@@ -35,9 +35,8 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            host: '0.0.0.0',
+            port: process.env.PORT
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
